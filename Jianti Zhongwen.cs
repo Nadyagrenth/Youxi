b5f77243820710ddac0adabd65fa8e730b5303e2
inan <nadyagrenth@gmail.com>
2025-07-15 14:49:55 +0300

    简体汉语

---
+++ Jianti Zhongwen.cs
@@ -0,0 +1,31 @@
+﻿{
+    "zh": {
+        "start_game": "开始游戏"
+    }
+}
+
+{
+    "zh-Hans": {
+        "start_game": "开始游戏",
+    "options": "选项",
+    "exit": "退出",
+    "resume": "继续",
+    "pause": "暂停",
+    "new_game": "新游戏",
+    "load_game": "加载游戏",
+    "settings": "设置",
+    "language": "语言",
+    "audio": "音频",
+    "music_volume": "音乐音量",
+    "sfx_volume": "音效音量",
+    "confirm": "确认",
+    "cancel": "取消",
+    "back": "返回",
+    "yes": "是",
+    "no": "否",
+    "game_over": "游戏结束",
+    "victory": "胜利",
+    "play_again": "再玩一次",
+    "main_menu": "主菜单"
+    }
+}
